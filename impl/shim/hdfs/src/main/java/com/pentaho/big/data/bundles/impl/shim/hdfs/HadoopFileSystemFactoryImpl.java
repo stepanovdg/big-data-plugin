f7736bdb6e4a229a0125c52fb572a0f651d1744b
Dzmitry_Stsiapanau <stepanovdg@gmail.com>
2017-01-13 11:37:18 +0300

    [BACKLOG-14108] - VFS changes to accomodate wasb

--- impl/shim/hdfs/src/main/java/com/pentaho/big/data/bundles/impl/shim/hdfs/HadoopFileSystemFactoryImpl.java
+++ impl/shim/hdfs/src/main/java/com/pentaho/big/data/bundles/impl/shim/hdfs/HadoopFileSystemFactoryImpl.java
@@ -1,23 +1,18 @@
 /*******************************************************************************
- *
  * Pentaho Big Data
- *
- * Copyright (C) 2002-2015 by Pentaho : http://www.pentaho.com
- *
- *******************************************************************************
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with
+ * <p>
+ * Copyright (C) 2002-2017by Pentaho : http://www.pentaho.com
+ * <p>
+ * ******************************************************************************
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  ******************************************************************************/
 
 package com.pentaho.big.data.bundles.impl.shim.hdfs;
@@ -75,10 +70,14 @@ public class HadoopFileSystemFactoryImpl implements HadoopFileSystemFactory {
       Variables variables = new Variables();
       variables.initializeVariablesFrom( null );
 
-      fsDefault = scheme + "://" + variables.environmentSubstitute( namedCluster.getHdfsHost() );
-      String port = variables.environmentSubstitute( namedCluster.getHdfsPort() );
-      if ( !Const.isEmpty( port ) ) {
-        fsDefault = fsDefault + ":" + port;
+      if ( scheme.contains( "://" ) ) {
+        fsDefault = scheme;
+      } else {
+        fsDefault = scheme + "://" + variables.environmentSubstitute( namedCluster.getHdfsHost() );
+        String port = variables.environmentSubstitute( namedCluster.getHdfsPort() );
+        if ( !Const.isEmpty( port ) ) {
+          fsDefault = fsDefault + ":" + port;
+        }
       }
       if ( fsDefault.endsWith( "//" ) ) {
         fsDefault += "/";
